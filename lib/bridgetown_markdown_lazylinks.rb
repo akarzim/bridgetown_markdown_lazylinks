5b87e2f7d0579e5e5d6284205afb476d363eddfd
François Vantomme <akarzim@pm.me>
2023-06-19 10:56:34 +0200

    refactor: add a mandatory plugin initializer

--- lib/bridgetown_markdown_lazylinks.rb
+++ lib/bridgetown_markdown_lazylinks.rb
@@ -2,3 +2,12 @@
 
 require "bridgetown"
 require "bridgetown_markdown_lazylinks/converter"
+
+# Mandatory plugin initializer
+#
+# @see https://github.com/bridgetownrb/bridgetown/issues/769#issuecomment-1596520674
+#
+# @param config [Bridgetown::Configuration::ConfigurationDSL]
+Bridgetown.initializer :bridgetown_markdown_lazylinks do |_|
+  # noop
+end
